4a6afeebd7bec28f8ba3ac2ccb8565bb0976e36b
shouwn <sjonghyon@gmail.com>
2018-06-26 13:49:02 +0900

    Initial commit

---
+++ src/main/java/test/MySpringLearningTestApplication.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringLearningTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringLearningTestApplication.class, args);
+	}
+}
