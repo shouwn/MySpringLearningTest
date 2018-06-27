51a13d1f7fee8603295cfb7434974e3aaa958152
shouwn <sjonghyon@gmail.com>
2018-06-27 17:54:36 +0900

    get 확인을 위해 2개로 수정

--- src/test/java/test/article/ArticleDAOTest.java
+++ src/test/java/test/article/ArticleDAOTest.java
@@ -21,18 +21,21 @@ public class ArticleDAOTest {
 				new ClassPathXmlApplicationContext("applicationContext.xml", TestConnectionMaker.class);
 
 		ArticleDAO articleDAO = context.getBean("articleDAO", ArticleDAO.class);
-		Article article = articleTestObject(215);
+		Article article1 = articleTestObject(215);
+		Article article2 = articleTestObject(216);
 		
 		int count = articleDAO.count();
-		articleDAO.delete(article.getId());
+		articleDAO.delete(article1.getId());
+		articleDAO.delete(article2.getId());
 
-		Assertions.assertEquals(count - 1, articleDAO.count());
+		Assertions.assertEquals(count - 2, articleDAO.count());
 
-		articleDAO.insertIncludeId(article);
-		Article other = articleDAO.findOne(article.getId());
-
+		articleDAO.insertIncludeId(article1);
+		articleDAO.insertIncludeId(article2);
+		
 		Assertions.assertEquals(count, articleDAO.count());
-		Assertions.assertEquals(article, other);
+		Assertions.assertEquals(article1, articleDAO.findOne(article1.getId()));
+		Assertions.assertEquals(article2, articleDAO.findOne(article2.getId()));
 	}
 	
 	@Test
