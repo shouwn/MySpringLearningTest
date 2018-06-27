c98a509517e3bafff0bcc86220e88cf1c984eec5
shouwn <sjonghyon@gmail.com>
2018-06-27 17:45:57 +0900

    count() test 작성

--- src/test/java/test/article/ArticleDAOTest.java
+++ src/test/java/test/article/ArticleDAOTest.java
@@ -5,10 +5,8 @@ import java.sql.Timestamp;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import article.DAOFactory;
 import article.TestConnectionMaker;
 import article.DAO.ArticleDAO;
 import article.dto.Article;
@@ -16,49 +14,54 @@ import article.dto.Article;
 public class ArticleDAOTest {
 
 	@Test
-	public void annotationConfigAddAndGet() throws Exception {
+	public void addAndGet() throws Exception {
 
 		ApplicationContext context =
-				new AnnotationConfigApplicationContext(DAOFactory.class);
-
-		ArticleDAO articleDAO = context.getBean("articleDAO", ArticleDAO.class);
-		Article article = articleTestObject();
-
-		articleDAO.delete(article.getId());
-		
-		Assertions.assertEquals(0, articleDAO.count());
-		
-		articleDAO.insertIncludeId(article);
-		Article other = articleDAO.findOne(article.getId());
-		
-		Assertions.assertEquals(article, other);
-		Assertions.assertEquals(1, articleDAO.count());
-	}
-
-	public static void xmlConfigTest() throws Exception {
-
-		ApplicationContext context =
+				//new AnnotationConfigApplicationContext(DAOFactory.class);
 				new ClassPathXmlApplicationContext("applicationContext.xml", TestConnectionMaker.class);
 
 		ArticleDAO articleDAO = context.getBean("articleDAO", ArticleDAO.class);
-		Article article = articleTestObject();
-
-		articleDAO.insertIncludeId(article);
-
-		Article other = articleDAO.findOne(article.getId());
-
-		System.out.println("insert: " + (article.equals(other) ? "success" : "fail"));
-
+		Article article = articleTestObject(215);
+		
+		int count = articleDAO.count();
 		articleDAO.delete(article.getId());
 
-		other = articleDAO.findOne(article.getId());
+		Assertions.assertEquals(count - 1, articleDAO.count());
 
-		System.out.println("delete: " + (other == null ? "success" : "fail"));
+		articleDAO.insertIncludeId(article);
+		Article other = articleDAO.findOne(article.getId());
+
+		Assertions.assertEquals(count, articleDAO.count());
+		Assertions.assertEquals(article, other);
+	}
+	
+	@Test
+	public void count() throws Exception {
+		ApplicationContext context =
+				//new AnnotationConfigApplicationContext(DAOFactory.class);
+				new ClassPathXmlApplicationContext("applicationContext.xml", TestConnectionMaker.class);
+
+		ArticleDAO articleDAO = context.getBean("articleDAO", ArticleDAO.class);
+		
+		int[] index = new int[] {0, 1, 2};
+		Article[] articles = new Article[3];
+		
+		for(int i : index) {
+			articles[i] = articleTestObject(220 + i);
+			articleDAO.delete(articles[i].getId());
+		}
+		
+		int count = articleDAO.count();
+
+		for(int i : index) {
+			articleDAO.insertIncludeId(articles[i]);
+			Assertions.assertEquals(count + i + 1, articleDAO.count());
+		}
 	}
 
-	public static Article articleTestObject() {
+	public Article articleTestObject(int id) {
 		Article article = new Article();
-		article.setId(215);
+		article.setId(id);
 		article.setNo(12345);
 		article.setTitle("TEST");
 		article.setBody("BODY FOR TEST");
