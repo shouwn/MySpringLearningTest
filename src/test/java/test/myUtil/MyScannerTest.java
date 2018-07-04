3e30b5e8b3819c76c90742cc4989e082963ebae7
shouwn <sjonghyon@gmail.com>
2018-07-05 03:18:27 +0900

    테스트 통과

--- src/test/java/test/myUtil/MyScannerTest.java
+++ src/test/java/test/myUtil/MyScannerTest.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -23,39 +25,36 @@ public class MyScannerTest {
 	@Test
 	public void nextTest() throws IOException {
 		
-		try(MyScanner<Integer> myScan = new MyScanner<>();
-				BufferedReader reader = new BufferedReader(new FileReader(testFilePath))) {
+		try(MyScanner<Integer> scan = new MyScanner<>()) {
 
-			myScan.setReader(new FileReader(testFilePath));
-			myScan.compile("[0-9]+", s -> Integer.valueOf(s));
+			Integer[] expected = getArrayByBufferedReader();
+			
+			scan.setReader(new FileReader(testFilePath));
+			scan.compile("[0-9]+", s -> Integer.valueOf(s));
 
+			List<Integer> list = new ArrayList<>();
+			
+			Integer i;
+			while((i = scan.next()) != null) {
+				list.add(i);
+			}
+			
+			Assertions.assertArrayEquals(expected, list.toArray(new Integer[0]));
 		}
 	}
 	
-	@Test
-	public void bufferedReaderTest() throws FileNotFoundException, IOException {
+	public Integer[] getArrayByBufferedReader() throws FileNotFoundException, IOException {
 		try(BufferedReader reader = new BufferedReader(new FileReader(testFilePath))){
-			int[] expected = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
+			Integer[] expected = new Integer[] {1, 2, 3, 4, 5, 6, 7, 8};
 			String[] line = reader.readLine().split(" ");
-			int[] actual = new int[line.length];
+			Integer[] actual = new Integer[line.length];
 			
 			for(int i = 0; i < actual.length; i++) 
 				actual[i] = Integer.valueOf(line[i]);
 			
 			Assertions.assertArrayEquals(expected, actual);
-		}
-	}
-	
-	public int[] getArrayByBufferedReader() {
-		try(BufferedReader reader = new BufferedReader(new FileReader(testFilePath))){
-			int[] expected = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
-			String[] line = reader.readLine().split(" ");
-			int[] actual = new int[line.length];
 			
-			for(int i = 0; i < actual.length; i++) 
-				actual[i] = Integer.valueOf(line[i]);
-			
-			Assertions.assertArrayEquals(expected, actual);
+			return actual;
 		}
 	}
 }
