2a2d1c1fe393c441d8d425a499396a8211f021e7
shouwn <sjonghyon@gmail.com>
2018-07-13 21:32:58 +0900

    메소드 레퍼런스 연습

--- src/test/java/test/java/MethodReferenceTest.java
+++ src/test/java/test/java/MethodReferenceTest.java
@@ -1,6 +1,25 @@
 package test.java;
 
+import java.util.function.Supplier;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MethodReferenceTest {
 	
-
+	private static final int ANS = 1000;
+	
+	@Test
+	public void staticMethod() {
+		Assertions.assertEquals(ANS, template(MethodReferenceTest::Method1));
+	}
+	
+	public int template(Supplier<Integer> supplier) {
+		return supplier.get();
+	}
+	
+	
+	public static int Method1() {
+		return ANS;
+	}
 }
